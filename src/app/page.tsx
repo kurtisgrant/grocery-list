2e35c2f4167cc16ed2cf07fee4fef92d9b0b4d3d
Kurtis Grant <kurtis.b.grant@gmail.com>
2024-01-19 15:43:04 -0500

    T3, prisma, next auth setup

---
+++ src/app/page.tsx
@@ -0,0 +1,45 @@
+import { unstable_noStore as noStore } from "next/cache";
+import Link from "next/link";
+
+import { CreatePost } from "~/app/_components/create-post";
+import { getServerAuthSession } from "~/server/auth";
+import { api } from "~/trpc/server";
+
+export default async function Home() {
+  noStore();
+  const hello = await api.post.hello.query({ text: "from tRPC" });
+  const session = await getServerAuthSession();
+
+  return (
+    <main className="flex">
+      <p className="text-2xl">
+        {session && <span>Logged in as {session.user?.name}</span>}
+      </p>
+      <Link
+        href={session ? "/api/auth/signout" : "/api/auth/signin"}
+        className="rounded-full bg-blue-500 px-10 py-3 font-semibold no-underline transition"
+      >
+        {session ? "Sign out" : "Sign in"}
+      </Link>
+    </main>
+  );
+}
+
+// async function CrudShowcase() {
+//   const session = await getServerAuthSession();
+//   if (!session?.user) return null;
+
+//   const latestPost = await api.post.getLatest.query();
+
+//   return (
+//     <div className="w-full max-w-xs">
+//       {latestPost ? (
+//         <p className="truncate">Your most recent post: {latestPost.name}</p>
+//       ) : (
+//         <p>You have no posts yet.</p>
+//       )}
+
+//       <CreatePost />
+//     </div>
+//   );
+// }
